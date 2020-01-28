b17468a0fbd3e389b8c7de9f17370df4856ca06a
Diana <5275194+DianaNites@users.noreply.github.com>
2020-01-28 00:04:23 -0500

    WIP Table-ish view

--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,9 @@
-use crate::util::get_disks;
+use crate::util::{get_disks, Disk};
 use cursive::{
     align::{HAlign, VAlign},
     event::Key,
     theme::{BaseColor::*, Color::*, PaletteColor::*, Theme},
     traits::Boxable,
-    view::{Finder, Nameable, Selector, View},
     views::{DummyView, LinearLayout, Panel, SelectView, TextView},
     Cursive,
 };
@@ -18,51 +17,71 @@ fn theme(root: &Cursive) -> Theme {
     theme
 }
 
-fn selection_screen(title: &str, list: impl View, name: &str) -> impl View {
-    Panel::new(
+fn parts_edit(root: &mut Cursive, _disk: &Disk) {
+    root.pop_layer();
+    //
+    let mut parts = SelectView::new();
+    parts.add_item("Dummy", ());
+    parts.add_item("Dummy", ());
+    //
+    let mut devices = LinearLayout::vertical();
+    devices.add_child(TextView::new("Device").h_align(HAlign::Left));
+    devices.add_child(DummyView);
+    // devices.add_child(TextView::new("Dummy Part 1").h_align(HAlign::Left));
+    // devices.add_child(TextView::new("Dummy Part 2").h_align(HAlign::Left));
+    // devices.add_child(TextView::new("Dummy Part 3").h_align(HAlign::Left));
+    devices.add_child(parts);
+
+    let mut starts = LinearLayout::vertical();
+    starts.add_child(TextView::new("Start").h_align(HAlign::Right));
+    starts.add_child(DummyView);
+    starts.add_child(TextView::new("1").h_align(HAlign::Right));
+    starts.add_child(TextView::new("2").h_align(HAlign::Right));
+    starts.add_child(TextView::new("3").h_align(HAlign::Right));
+    //
+    root.add_fullscreen_layer(
+        Panel::new(
+            LinearLayout::vertical()
+                .child(
+                    LinearLayout::horizontal()
+                        .child(devices)
+                        .child(DummyView.full_width())
+                        .child(starts)
+                        // .child(TextView::new("Device").h_align(HAlign::Left).full_width())
+                        // .child(TextView::new("Start").h_align(HAlign::Center).full_width())
+                        .child(TextView::new("End").h_align(HAlign::Center).full_width())
+                        .child(TextView::new("Size").h_align(HAlign::Center).full_width())
+                        .child(TextView::new("Type").h_align(HAlign::Center).full_width()),
+                )
+                .child(DummyView), // .child(parts),
+        )
+        .full_screen(),
+    );
+}
+
+fn disk_selection(root: &mut Cursive) {
+    let mut disks = SelectView::new().h_align(HAlign::Center);
+    disks.add_all(get_disks());
+    disks.set_on_submit(parts_edit);
+    //
+    let select = Panel::new(
         LinearLayout::vertical()
             .child(TextView::new(format!(
                 "Part {}, {}",
                 std::env!("CARGO_PKG_VERSION"),
                 std::env!("CARGO_PKG_DESCRIPTION")
             )))
-            .child(TextView::new(title).h_align(HAlign::Center))
+            .child(TextView::new("Select A Disk").h_align(HAlign::Center))
             .child(DummyView)
-            .child(list)
-            .with_name(name),
-    )
-    .full_screen()
-}
-
-fn parts_edit(_root: &mut Cursive) -> impl View {
-    let mut parts = SelectView::new();
-    parts.add_item("Dummy            a", ());
-    parts.add_item("Dummy", ());
-    parts
-}
-
-fn disk_selection(root: &mut Cursive) {
-    let mut disks = SelectView::new().h_align(HAlign::Center);
-    disks.add_all(get_disks());
-    disks.set_on_submit(|root, _| {
-        root.pop_layer();
-        //
-        let parts = parts_edit(root);
-        //
-        root.add_fullscreen_layer(selection_screen("Select a Partition", parts, "Part List"))
-    });
-    //
-    let mut select = selection_screen("Select a Disk", disks, "Disk List");
-    select
-        .call_on(&Selector::Name("Disk List"), |list: &mut LinearLayout| {
-            list.add_child(DummyView);
-            list.add_child(
+            .child(disks)
+            .child(DummyView)
+            .child(
                 TextView::new("If Disk Capacity is incorrect DO NOT continue")
                     .v_align(VAlign::Bottom)
                     .full_screen(),
-            )
-        })
-        .unwrap();
+            ),
+    )
+    .full_screen();
 
     root.add_fullscreen_layer(select)
 }
