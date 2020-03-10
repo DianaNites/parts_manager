cda922a6c13a3a998a2ff8317f24b190c784757d
Diana <5275194+DianaNites@users.noreply.github.com>
2020-03-10 13:02:21 -0400

    WIP cli interface

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,15 @@ use views::*;
 #[structopt(global_setting(AppSettings::ColoredHelp))]
 struct Args {
     /// Path to device or file to partition.
-    ///
-    /// If not provided, you can choose between connected block devices.
-    device: Option<PathBuf>,
+    #[structopt(default_value = "/dev/sda")]
+    device: PathBuf,
 
-    /// Logical Block Size to use. Ignored when `device` is not specified.
-    #[structopt(long, default_value("512"))]
-    block: u64,
+    /// Logical Block Size to use. Overrides autodetection from `device`.
+    #[structopt(long)]
+    block: Option<u64>,
 }
 
-fn main() -> Result<()> {
-    let args: Args = Args::from_args();
-    //
+fn interactive() -> Result<()> {
     let mut root = Cursive::default();
     // Theme
     let mut theme = root.current_theme().clone();
@@ -41,32 +38,28 @@ fn main() -> Result<()> {
     theme.palette[Tertiary] = Dark(White);
     root.set_theme(theme);
 
-    // User Entry point
-    match args.device {
-        Some(path) => {
-            let block_size = BlockSize(args.block);
-            let disk_size = ByteSize::from_bytes(fs::metadata(&path).unwrap().len());
-            let name = path.file_name().unwrap().to_str().unwrap().to_owned();
-            root.add_fullscreen_layer(part_view(&Data::from_path(
-                path, name, block_size, disk_size,
-            )?)?);
-            // Info box will start empty, make sure callback is called and it's set.
-            root.call_on_name("parts", |v: &mut SelectView<Partition>| v.set_selection(0))
-                .unwrap()(&mut root);
-        }
-        None => {
-            root.add_fullscreen_layer(disks()?);
-            // Disk Info box will start empty, make sure callback is called and it's set.
-            root.call_on_name("disks", |v: &mut SelectView<Data>| v.set_selection(0))
-                .unwrap()(&mut root);
-        }
-    }
+    root.add_fullscreen_layer(disks()?);
+    // Disk Info box will start empty, make sure callback is called and it's set.
+    root.call_on_name("disks", |v: &mut SelectView<Data>| v.set_selection(0))
+        .unwrap()(&mut root);
 
     // Global hotkeys
     root.add_global_callback('q', |s| s.quit());
     root.add_global_callback('h', |_| todo!("Help menu"));
     //
     root.run();
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args: Args = Args::from_args();
+    //
+    let path = args.device;
+    let block_size = BlockSize(args.block.unwrap());
+    let disk_size = ByteSize::from_bytes(fs::metadata(&path).unwrap().len());
+    let name = path.file_name().unwrap().to_str().unwrap().to_owned();
+    //
+    let data = Data::from_path(path, name, block_size, disk_size)?;
     //
     Ok(())
 }
