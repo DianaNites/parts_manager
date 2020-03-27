9fa1213fa00f49b5a52dadc51f0ab95ad1684ae6
Diana <5275194+DianaNites@users.noreply.github.com>
2020-03-27 15:05:08 -0400

    simplify

--- src/views.rs
+++ src/views.rs
@@ -196,7 +196,7 @@ pub fn disks() -> Result<impl View> {
         );
         disks_view.add_item(label, get_info_block(&disk)?);
     }
-    disks_view.set_on_submit(|mut root, info| {
+    disks_view.set_on_submit(|root, info| {
         let file = fs::OpenOptions::new()
             .read(true)
             .write(true)
@@ -208,17 +208,17 @@ pub fn disks() -> Result<impl View> {
                     Ok(gpt) => {
                         root.add_fullscreen_layer(parts(gpt, info));
                         root.call_on_name("parts", |v: &mut PartSelect| v.set_selection(0))
-                            .expect("Missing callback")(&mut root);
+                            .expect("Missing callback")(root);
                     }
                     Err(e) => {
                         let info = info.clone();
-                        let dialog = error(e).button("New GPT", move |mut root| {
+                        let dialog = error(e).button("New GPT", move |root| {
                             let gpt: Gpt =
                                 Gpt::new(Uuid::new_v4(), info.disk_size, info.block_size);
                             root.pop_layer();
                             root.add_fullscreen_layer(parts(gpt, &info));
                             root.call_on_name("parts", |v: &mut PartSelect| v.set_selection(0))
-                                .expect("Missing callback")(&mut root);
+                                .expect("Missing callback")(root);
                         });
                         root.add_layer(dialog);
                     }
